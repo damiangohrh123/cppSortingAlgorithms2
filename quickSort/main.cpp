85d188a74c781613c906c6c5230dbd90fbda9d5a
Damian <damiangohrh@gmail.com>
2024-09-20 20:29:59 +0800

    Implement quicksort

---
+++ quickSort/main.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = arr[high];
+        int leftIndex = low;
+
+        for (int i = low; i < high; i++)
+        {
+            if (arr[i] < pivot)
+            {
+                std::swap(arr[leftIndex], arr[i]);
+                leftIndex++;
+            }
+        }
+        std::swap(arr[leftIndex], arr[high]);
+
+        quickSort(arr, low, leftIndex - 1);
+        quickSort(arr, leftIndex + 1, high);
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size, std::string message)
+{
+    std::cout << message << "\n";
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+
+    std::cout << "\n";
+}
+
+int main()
+{
+    int arr1[] = {5, 1, 4, 8, 7, 2};
+    int arr1Length = sizeof(arr1) / sizeof(arr1[0]);
+
+    int arr2[] = {9, 3, 8, 6, 1};
+    int arr2Length = sizeof(arr2) / sizeof(arr2[0]);
+
+    printArray(arr1, arr1Length, "Unsorted Array: ");
+    quickSort(arr1, 0, arr1Length - 1);
+    printArray(arr1, arr1Length, "QuickSort: ");
+
+    printArray(arr2, arr2Length, "Unsorted Array: ");
+    quickSort(arr2, 0, arr2Length - 1);
+    printArray(arr2, arr2Length, "QuickSort: ");
+
+    return 0;
+}
