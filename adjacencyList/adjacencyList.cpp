7d85bbc45e5a7ddf5629197117d8abfe035e9f49
Damian <damiangohrh@gmail.com>
2024-10-02 22:28:13 +0800

    Implement Prims algorithm for MST

--- adjacencyList/adjacencyList.cpp
+++ adjacencyList/adjacencyList.cpp
@@ -2,22 +2,23 @@
 
 struct Node {
     int data;
+    int weight;
     Node* next;
 
-    Node(int val) : data(val), next(nullptr) {}
+    Node(int val, int _weight) : data(val), weight(_weight), next(nullptr) {}
 };
 
 class AdjacencyList {
 private:
-    int vertices = 5;
+    int vertices;
     std::vector<Node*> list;
 
 public:
-    AdjacencyList();
+    AdjacencyList(int v);
     ~AdjacencyList();
 
     // Adds an edge to the list
-    void addEdge(int i, int j);
+    void addEdge(int i, int j, int weight);
 
     // Removes an edge from the list
     void removeEdge(int i, int j);
@@ -27,17 +28,23 @@ public:
 
     // Print the list
     void display();
+
+    // Return the adjacency list with a vertex index as input
+    Node* returnList(int vertex);
+
+    // Return number of vertices of graph
+    int returnVertices();
 };
 
 // Initialize the list with nullptrs
-AdjacencyList::AdjacencyList() : list(vertices, nullptr) {}
+AdjacencyList::AdjacencyList(int v) : vertices(v), list(v, nullptr) {}
 AdjacencyList::~AdjacencyList() {}
 
-void AdjacencyList::addEdge(int i, int j) {
+void AdjacencyList::addEdge(int i, int j, int weight) {
     // Make sure i and j is within bounds
     if (i < 0 || j < 0 || i >= vertices || j >= vertices) return;
 
-    Node* newNode = new Node(j);
+    Node* newNode = new Node(j, weight);
     newNode->next = list[i];
     list[i] = newNode;
 }
@@ -101,16 +108,91 @@ void AdjacencyList::display() {
     }
 }
 
+Node* AdjacencyList::returnList(int vertex) {
+    return list[vertex];
+}
+
+int AdjacencyList::returnVertices() {
+    return vertices;
+}
+
+class PrimMST {
+private:
+    int vertices;
+    std::vector<int> parent;   // To store the parent of each vertex
+    std::vector<int> key;      // To store the minimum weight to add each vertex
+    std::vector<bool> visited; // To track vertices already included in the MST
+
+public:
+    PrimMST(int v); // Constructor to initialize the arrays and graph size
+
+    // Create tyhe MST
+    void findMST(AdjacencyList& graph);
+
+    // Display the MST
+    void displayMST();
+};
+
+// Initialize parent vector
+// Initialize the key vector to have "infinity" values. "Key" is the weight of edge.
+// Initialize visited vector
+PrimMST::PrimMST(int v) : vertices(v), parent(v, -1), key(v, std::numeric_limits<int>::max()), visited(v, false) {}
+
+void PrimMST::findMST(AdjacencyList& graph) {
+    // Set the key for the starting vertex (0)
+    key[0] = 0;
+
+    // Min-heap to store pairs (key value, vertex)
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> minHeap;
+    minHeap.push(std::make_pair(key[0], 0)); // Push 1st vertex into min heap
+    
+    while (!minHeap.empty()) {
+        // Get the vertex from the min-heap
+        int vertex = minHeap.top().second;
+        minHeap.pop(); // Remove it from the heap
+
+        // Mark the vertex as visited
+        visited[vertex] = true;
+
+        // Explore the neighbours and update the keys' weights
+        Node* current = graph.returnList(vertex);
+        while (current) {
+            int neighbor = current->data;
+            int weight = current->weight;
+
+            // If vertex has not been visited, and weight is lesser than current key, update the key to the weight
+            if (!visited[neighbor] && weight < key[neighbor]){
+                key[neighbor] = weight; // Update the key
+                parent[neighbor] = vertex; // Update parent
+                minHeap.push(std::make_pair(key[neighbor], neighbor)); // Push updated key to min-heap
+            }
+            current = current->next;
+        }
+    }
+}
+
+void PrimMST::displayMST() {
+    std::cout << "Edge    Weight\n";
+    for (int i=1; i<parent.size(); i++) {
+        std::cout << parent[i] << " - " << i << "\t" << key[i] << "\n";
+    }
+}
+
 int main() {
-    AdjacencyList* list = new AdjacencyList;
-    list->addEdge(0, 3);
-    list->addEdge(2, 4);
-    list->addEdge(1, 2);
-    list->addEdge(1, 3);
-    list->addEdge(1, 4);
-    list->addEdge(4, 1);
-    list->removeEdge(4, 1);
-    list->display();
+    AdjacencyList graph(6);
+    graph.addEdge(0, 1, 4);
+    graph.addEdge(0, 2, 6);
+    graph.addEdge(1, 2, 6);
+    graph.addEdge(1, 3, 3);
+    graph.addEdge(1, 4, 4);
+    graph.addEdge(2, 3, 1);
+    graph.addEdge(3, 4, 2);
+    graph.addEdge(3, 5, 3);
+    graph.addEdge(4, 5, 7);
+    graph.display();
+    PrimMST mst(graph.returnVertices());
+    mst.findMST(graph);
+    mst.displayMST();
     return 0;
 }
 
