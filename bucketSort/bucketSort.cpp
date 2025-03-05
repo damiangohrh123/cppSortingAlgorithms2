30fae4a473ff8068dc1b3b19e963556b3a70009d
Damian <damiangohrh@gmail.com>
2025-03-05 19:53:12 +0800

    Refactor code

--- bucketSort/bucketSort.cpp
+++ bucketSort/bucketSort.cpp
@@ -1,70 +1,66 @@
 #include <bits/stdc++.h>
 
-std::vector<int> bucketSort(std::vector<int> &arr)
-{
-    // Create buckets. Each bucket is a linked list. Sqrt to make bucket smaller.
-    std::vector<std::list<int>> bucket(sqrt(arr.size()));
-
-    // Get max value
-    int maxVal = arr[0];
-    for (int i = 1; i < arr.size(); i++)
-    {
-        if (arr[i] > maxVal)
-        {
-            maxVal = arr[i];
+void insertionSort(std::vector<int>& vec) {
+    for (int i=0; i<vec.size(); i++) {
+        for (int j=i; j>0; j--) {
+            if (vec[j] < vec[j-1]) {
+                int temp = vec[j];
+                vec[j] = vec[j-1];
+                vec[j-1] = temp;
+            }
         }
     }
-
-    // Hash function
-    for (int i = 0; i < arr.size(); i++)
-    {
-        int bucketIndex = (arr[i] * bucket.size()) / (maxVal + 1);
-
-        // Push value into bucket. It will automatically be placed at the end of the linked list.
-        bucket[bucketIndex].push_back(arr[i]);
-    }
-
-    std::vector<int> sortedArr;
-
-    // Loop through bucket
-    for (int i = 0; i < bucket.size(); i++)
-    {
-        // Loop through each linked list in reverse
-        for (auto it = bucket[i].rbegin(); it != bucket[i].rend(); ++it)
-        {
-            sortedArr.push_back(*it);
-        }
-    }
-
-    arr = sortedArr;
-    return arr;
 }
 
-// Function to print an array
-void printArray(std::vector<int> arr, int size, std::string message)
-{
-    std::cout << message << "\n";
+std::vector<int> bucketSort(std::vector<int>& vec) {
+    // Create bucket
+    std::vector<std::vector<int>> bucket(vec.size());
 
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << arr[i] << " ";
+    // Get max value
+    int maxVal = vec[0];
+    for (int i=1; i<vec.size(); i++) {
+        if (vec[i] > maxVal) {
+            maxVal = vec[i];
+        }
     }
 
+    // Place values into bucket
+    for (int i=0; i<vec.size(); i++) {
+        int bucketIndex = (vec[i] * bucket.size() / (maxVal + 1));
+        bucket[bucketIndex].push_back(vec[i]);
+    }
+
+    std::vector<int> sortedVec;
+
+    // Loop through buckets, sort values using insertionSort, and add sorted values to sortedVec
+    for (int i=0; i<bucket.size(); i++) {
+        if (bucket[i].size() > 1) {
+            insertionSort(bucket[i]);
+        }
+
+        for (int j=0; j<bucket[i].size(); j++) {
+            sortedVec.push_back(bucket[i][j]);
+        }
+    }
+
+    return sortedVec;
+}
+
+void printArray(std::vector<int>& vec) {
+    for (int i=0; i<vec.size(); i++) {
+        std::cout << vec[i] << " ";
+    }
     std::cout << "\n";
 }
 
 int main()
 {
-    std::vector<int> arr1 = {5, 1, 4, 8, 7, 2};
-    std::vector<int> arr2 = {613, 218, 350, 157, 108, 457};
+    std::vector<int> myVec = {2, 8, 5, 3, 9, 4};
+    std::cout << "Before Sorting: ";
+    printArray(myVec);
 
-    printArray(arr1, arr1.size(), "Unsorted Array: ");
-    arr1 = bucketSort(arr1);
-    printArray(arr1, arr1.size(), "Bucket Sort: ");
-
-    printArray(arr2, arr2.size(), "Unsorted Array: ");
-    arr2 = bucketSort(arr2);
-    printArray(arr2, arr2.size(), "Bucket Sort: ");
-
+    std::vector<int> myVecSorted = bucketSort(myVec);
+    std::cout << "After Sorting: ";
+    printArray(myVecSorted);
     return 0;
 }
