d0a6156560e3ff837194f3c1d42f1ab850af4bc1
damiangohrh123 <damiangohrh@gmail.com>
2025-03-31 22:19:00 +0800

    Refactor quickSort

--- quickSort/quickSort.cpp
+++ quickSort/quickSort.cpp
@@ -1,48 +1,43 @@
 #include <bits/stdc++.h>
 
-void quickSort(int arr[], int low, int high)
-{
+void quickSort(std::vector<int>& n, int low, int high) {
     if (low < high)
     {
-        int pivot = arr[high];
+        int pivot = n[high];
         int leftIndex = low;
 
         for (int i = low; i < high; i++)
         {
-            if (arr[i] < pivot)
+            if (n[i] < pivot)
             {
-                std::swap(arr[leftIndex], arr[i]);
+                std::swap(n[leftIndex], n[i]);
                 leftIndex++;
             }
         }
-        std::swap(arr[leftIndex], arr[high]);
+        std::swap(n[leftIndex], n[high]);
 
-        quickSort(arr, low, leftIndex - 1);
-        quickSort(arr, leftIndex + 1, high);
+        quickSort(n, low, leftIndex - 1);
+        quickSort(n, leftIndex + 1, high);
     }
 }
 
 // Function to print an array
-void printArray(int arr[], int size, std::string message)
-{
-    std::cout << message << "\n";
-
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << arr[i] << " ";
+void printArray(std::vector<int>& vec) {
+    for (int i=0; i<vec.size(); i++) {
+        std::cout << vec[i] << " ";
     }
-
     std::cout << "\n";
 }
 
 int main()
 {
-    int arr1[] = {5, 1, 4, 8, 7, 2};
-    int arr1Length = sizeof(arr1) / sizeof(arr1[0]);
+    std::vector<int> myVec = {221, 8, 54, 33, 991, 4, 1};
+    std::cout << "Before Sorting: ";
+    printArray(myVec);
 
-    printArray(arr1, arr1Length, "Unsorted Array: ");
-    quickSort(arr1, 0, arr1Length - 1);
-    printArray(arr1, arr1Length, "QuickSort: ");
+    quickSort(myVec, 0, myVec.size());
+    std::cout << "After Sorting: ";
+    printArray(myVec);
 
     return 0;
 }
