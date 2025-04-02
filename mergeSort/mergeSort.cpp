c4d644c16ec61f9547affc854cc62584d784f6f9
damiangohrh123 <damiangohrh@gmail.com>
2025-04-02 18:13:02 +0800

    Refactor mergeSort.

--- mergeSort/mergeSort.cpp
+++ mergeSort/mergeSort.cpp
@@ -1,80 +1,60 @@
 #include <bits/stdc++.h>
 
-std::vector<int> merge (std::vector<int> left, std::vector<int> right)
-{
-    // Merged Array
-    std::vector<int> mergedArr;
+std::vector<int> merge (std::vector<int> left, std::vector<int> right) {
+    std::vector<int> sorted;
+    int l = 0;
+    int r = 0;
 
-    // Indexes to loop thorugh each array
-    int l = 0, r = 0;
-
-    while (l < left.size() && r < right.size())
-    {
-        if (left[l] < right[r])
-        {
-            mergedArr.push_back(left[l]);
+    // Sort and merge back
+    while (l < left.size() && r < right.size()) {
+        if (left[l] < right[r]) {
+            sorted.push_back(left[l]);
             l++;
-        }
-        else 
-        {
-            mergedArr.push_back(right[r]);
+        } else {
+            sorted.push_back(right[r]);
             r++;
         }
     }
 
-    // If left array is exhausted
-    while (r < right.size())
-    {
-        mergedArr.push_back(right[r]);
-        r++;
-    }
+    // Push back any leftovers
+    sorted.insert(sorted.end(), left.begin() + l, left.end());
+    sorted.insert(sorted.end(), right.begin() + r, right.end());
 
-    // If right array is exhausted
-    while (l < left.size() && r >= right.size())
-    {
-        mergedArr.push_back(left[l]);
-        l++;
-    }
-
-    return mergedArr;
+    return sorted;
 }
 
-std::vector<int> mergeSort(std::vector<int>& arr, int low, int high)
-{
+std::vector<int> mergeSort(std::vector<int>& n, int low, int high) {
+
     // Base case
-    if (low >= high)
-    {
-        return {arr[low]};
-    }
+    if (low >= high) return {n[low]};
 
-    int mid = low + (high - low) / 2;
+    // Split the vector recursively until it has a single element
+    int mid = low + (high-low) / 2;
+    std::vector<int> left = mergeSort(n, low, mid);
+    std::vector<int> right = mergeSort(n, mid+1, high);
 
-    // Split array in halves
-    std::vector<int> left = mergeSort(arr, low, mid);
-    std::vector<int> right = mergeSort(arr, mid + 1, high);
+    // Merge the vectors back togrhter while sorting
     return merge(left, right);
 }
 
+
 // Function to print an array
-void printArray(std::vector<int> arr, int size, std::string message)
-{
-    std::cout << message << "\n";
-
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << arr[i] << " ";
+void printArray(std::vector<int>& vec) {
+    for (int i=0; i<vec.size(); i++) {
+        std::cout << vec[i] << " ";
     }
-
     std::cout << "\n";
 }
 
 int main()
 {
-    std::vector<int> arr1 = {5, 1, 4, 8, 7, 2};
+    std::vector<int> myVec = {221, 8, 54, 33, 991, 4, 1};
+    std::cout << "Before Sorting: ";
+    printArray(myVec);
 
-    printArray(arr1, arr1.size(), "Unsorted Array: ");
-    arr1 = mergeSort(arr1, 0, arr1.size() - 1);
-    printArray(arr1, arr1.size(), "Merge Sort: ");
+    std::vector<int> sortedVec = mergeSort(myVec, 0, myVec.size() - 1);
+    std::cout << "After Sorting: ";
+    printArray(sortedVec);
 
     return 0;
 }
